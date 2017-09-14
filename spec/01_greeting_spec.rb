db648c334330121cd46866d0f53e79916244d55f
Bhabya  Mishra <mbhabya@gmail.com>
2017-09-14 17:59:04 +0000

    automatically backed up by learn

--- spec/01_greeting_spec.rb
+++ spec/01_greeting_spec.rb
@@ -20,6 +20,7 @@ require_relative "../lib/greeting.rb"
     #  expect{greeting("Bobby")}.to_not raise_error
 def mymethod
   puts "Hi! I'm HAL, what's your name?"
+  puts "Please enter your name so that we can greet you personally"
   name = gets.strip
   greeting("Don")
 end
